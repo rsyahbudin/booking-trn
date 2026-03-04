bd52e0a32e169fc9b91031bf6756430d5c2f4e48
rsyahbudin <syahbudin45@gmail.com>
2026-03-04 09:23:27 +0700

    update rules

--- config/booking.php
+++ config/booking.php
@@ -39,6 +39,6 @@ return [
         'Pemesanan menu tambahan di luar Paket Ramadhan (ala carte) dapat dilakukan di tempat mulai pukul 19.00 WIB.',
         'Harga paket sudah mencakup biaya Booking Fee, Takjil, dan Free Flow Es Teh dan Infused Water / Mineral water',
         'Batas waktu reservasi untuk hari yang sama adalah pukul 15.00 WIB. (Untuk pengecekan ketersediaan tempat setelah jam tersebut, silakan hubungi WhatsApp kami di +62 858-1303-5292.',
-        'Pembatalan booking maksimal H-1 sebelum tanggal booking.',
+        'Refund pembatalan: H-7 dipotong 25% dari DP, H-3 dipotong 50% dari DP, dan H-1 dipotong 100% dari DP (DP hangus).',
     ],
 ];
